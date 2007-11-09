18f81be9e297ac881c1d7c24d066b3747fc24952
Sjur N Moshagen <sjurnm@mac.com>
2007-11-09 19:23:29 +0000

    Added support files for paradigm testing in SMJ.

---
+++ testing/gen-paradigms.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Shell script to run paradigm generation for a list of words
+# It first cleans a bit, and then calls a perl script that does
+# the processing of the word list and calls make.
+
+rm -f paradigm-smj.txt
+rm -f *.paradigm
+perl gen-paradigms.pl parawlist.txt
