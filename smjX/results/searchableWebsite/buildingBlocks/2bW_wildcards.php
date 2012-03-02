41c01b10c5146a4e151d5ba056f43703039e191b
Joshua Wilbur <jeutzsch@gmail.com>
2012-03-02 05:24:36 +0000

    added modification to search interface
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/smj@54607 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ smjX/results/searchableWebsite/buildingBlocks/2bW_wildcards.php
@@ -0,0 +1,11 @@
+</form>
+<tr><td colspan="4"><?php if ($_GET['regExps'] != "y") echo "<a href='searchSMJ_SQL_longReg.php?regExps=y' target='_self'><button type='button'>search using regular expressions</button></a>"; ?></td></tr>
+<tr><td colspan='4'>
+<table border="0" class='searchCrits' style='text-align:left;width:290px;'>
+<tr><td colspan='3' >wildcards:</td></tr>
+<tr style='font-style:normal;'><td>%</td><td>=</td><td>zero or more characters</td></tr>
+<tr style='font-style:normal;'><td>_</td><td>=</td><td>any single character</td></tr>
+</table>
+</td></tr>
+</table>
+</div>
