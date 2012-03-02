ccca2f2f2cc29f592b8d0466f4d4c54f19d12955
Joshua Wilbur <jeutzsch@gmail.com>
2012-03-02 05:21:23 +0000

    added addition search interface with regular expressions
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/smj@54603 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ smjX/results/searchableWebsite/searchSMJ_SQL_longReg.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name='keywords' content='Lule Saami, Mávsulasj'/>
+  <link href='buildingBlocks/searchable_PHP_SQL.css' rel='stylesheet' type='text/css'/>
+  <link rel='shortcut icon' href='buildingBlocks/farben4.gif'/>
+  <title>search Mávsulasj (advanced)</title>
+  <script type="text/javascript" src="buildingBlocks/audioFiles/jwplayer/jwplayer.js"></script>
+<!-- script for pop up of regular expressions: -->
+        <script type="text/javascript">
+        <!--
+        function popup(mylink, windowname)
+        {
+        if (! window.focus)return true;
+        var href;
+        if (typeof(mylink) == 'string')
+           href=mylink;
+        else
+           href=mylink.href;
+        window.open(href, windowname, 'width=420px,height=450px,');
+        return false;
+        }
+        //-->
+        </script>
+</head>
+<body bgcolor="#FFFFFF">
+
+<?php include("buildingBlocks/1_mainHeader.php"); ?>     
+
+<?php include("buildingBlocks/2b_searchFieldsLong.php"); ?>     
+
+<?php include("buildingBlocks/2bR_regularExpressions.php"); ?>     
+
+<?php include("buildingBlocks/3_credits.php"); ?>     
+
+<?php include("buildingBlocks/4b_detailsLong.php"); ?>     
+
+<?php include("buildingBlocks/5_results.php"); ?>     
+
+
+</body>
+</html>
