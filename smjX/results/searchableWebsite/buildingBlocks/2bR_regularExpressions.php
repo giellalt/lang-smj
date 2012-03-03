42dd1b69d35818af7f25445661d138515d5b822f
Joshua Wilbur <jeutzsch@gmail.com>
2012-03-03 17:50:44 +0000

    added links, text
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/smj@54677 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- smjX/results/searchableWebsite/buildingBlocks/2bR_regularExpressions.php
+++ smjX/results/searchableWebsite/buildingBlocks/2bR_regularExpressions.php
@@ -1,34 +1,12 @@
-<!--<tr><td colspan="4"><p>in regular expression mode</p></td></tr>-->
-
 <input type="hidden" name="regExps" value="y"></input>
 <tr><td colspan="4"><a href='searchSMJ_SQL_long.php' target='_self'><button type='button'>return to normal search mode</button></a></td></tr>
 
 </form>
-<tr><td colspan='4'>
-<a href="buildingBlocks/7_regExpPopUp.html" onClick="return popup(this, 'regular expressions')">see a list of regular expressions</a>
-<!--<table border="1" class='searchCrits' style='text-align:left;width:290px;'>
-<tr><td colspan='3' >regular expressions:</td></tr>
-<tr style='font-style:normal;'><td>.</td><td>=</td><td>match any character</td></tr>
-<tr style='font-style:normal;'><td>?</td><td>=</td><td>match zero or one character</td></tr>
-<tr style='font-style:normal;'><td>*</td><td>=</td><td>match zero or more characters</td></tr>
-<tr style='font-style:normal;'><td>+</td><td>=</td><td>match one or more characters</td></tr>
-<tr style='font-style:normal;'><td>{n}</td><td>=</td><td>match <span style='font-style:italic;'>n</span> times</td></tr>
-<tr style='font-style:normal;'><td>{m,n}</td><td>=</td><td>match <span style='font-style:italic;'>m</span> through <span style='font-style:italic;'>n</span> times</td></tr>
-<tr style='font-style:normal;'><td>{n,}</td><td>=</td><td>match <span style='font-style:italic;'>n or more</span> times</td></tr>
-<tr style='font-style:normal;'><td>^</td><td>=</td><td>beginning of a line</td></tr>
-<tr style='font-style:normal;'><td>$</td><td>=</td><td>end of a line</td></tr>
-<tr style='font-style:normal;'><td>[abc]</td><td>=</td><td>match one of enclosed characters</td></tr>
-<tr style='font-style:normal;'><td>[^abc]</td><td>=</td><td>exclude enclosed characters</td></tr>
-<tr style='font-style:normal;'><td>[:class:]</td><td>=</td><td>match a character class, i.e.:</td></tr>
-<tr style='font-style:normal;'><td></td><td></td><td>[:alpha:] for letters</td></tr>
-<tr style='font-style:normal;'><td></td><td></td><td>[:space:] for white space</td></tr>
-<tr style='font-style:normal;'><td></td><td></td><td>[:punct:] for punctuation</td></tr>
-<tr style='font-style:normal;'><td></td><td></td><td>[:upper:] for upper-case letters</td></tr>
-<tr style='font-style:normal;'><td>[[:<:]]</td><td>=</td><td>match beginning of words</td></tr>
-<tr style='font-style:normal;'><td>[[:>:]]</td><td>=</td><td>match ending of words</td></tr>
-<tr style='font-style:normal;'><td>|</td><td></td><td>used to separate alternatives</td></tr>
-</table>-->
 </td></tr>
 </table>
-
+<p class="searchCrits" style="text-align:left;width:200px;">more on regular expressions:<br/>
+<tr><td colspan='4'>
+<a href="buildingBlocks/7_regExpPopUp.html" onClick="return popup(this, 'regular expressions')">• summary</a><br/>
+<a href="https://dev.mysql.com/doc/refman/5.1/en/regexp.html" target="_blank">• detailed description</a>
+</p>
 </div>
