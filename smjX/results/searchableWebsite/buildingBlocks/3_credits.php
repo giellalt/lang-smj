a5c2334be2c7f1ed754ea209ec71a6951e0880f3
jeutzsch <jeutzsch@gmail.com>
2012-03-12 18:14:54 +0000

    added to credits

--- smjX/results/searchableWebsite/buildingBlocks/3_credits.php
+++ smjX/results/searchableWebsite/buildingBlocks/3_credits.php
@@ -1,14 +1,16 @@
 <script>
 <!--
-document.write("<div style='height:80px;width:300px;padding:10px;margin-left:"+leftOffset+"px;background-color:#FFFFFF;position:fixed;bottom:4px;'>");
+document.write("<div style='height:96px;width:300px;padding:10px;margin-left:"+leftOffset+"px;background-color:#FFFFFF;position:fixed;bottom:4px;'>");
 //-->
 </script>
 
-<!--div style="height:80px;width:320px;padding:10px;margin-left:960px;background-color:#FFFFFF;position:fixed;bottom:4px;"-->
+<!--div style="height:96px;width:320px;padding:10px;margin-left:960px;background-color:#FFFFFF;position:fixed;bottom:4px;"-->
 <p class="table" style="text-align:right;">
 <br/>Please contact<br/>
 <span style="color:red;font-family:'Courier'">mavsulasj(at)gmx.net</span>
-<br/>for questions or feedback<br/>
-<span style="font-style:italic;">Designed by Joshua Wilbur</span>
+<br/>with questions or feedback<br/>
+<span style="font-style:italic;">Designed by <a style='color:black;' href='http://jwilbur.de/' target='_blank'>Joshua Wilbur</a>
+<!--part of <br/>-->
+for</span><br/><a href='http://castl.uit.no/index.php?option=com_content&view=article&id=204&Itemid=244' target='_blank'><span style='font-style:italic;'>Mávulasj</span> Spoken Lule Saami Documentation Project</a>
 </p>
 </div>
