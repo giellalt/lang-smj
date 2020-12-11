bb288e1578ee574bc935dfabb55c0306d0383c34
Trond Trosterud <trond.trosterud@uit.no>
2020-12-11 16:08:10 +0100

    Remove in.txt after run

--- devtools/prop_minip.sh
+++ devtools/prop_minip.sh
@@ -23,5 +23,6 @@ do
  do
    echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smj/src/generator-gt-norm.hfstol
  done
+ rm -f $L_FILE
 done
 
--- devtools/verb_minip.sh
+++ devtools/verb_minip.sh
@@ -22,5 +22,6 @@ do
  do
    echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smj/src/generator-gt-norm.hfstol
  done
+ rm -f $L_FILE
 done
 
