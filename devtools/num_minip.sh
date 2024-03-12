a2980962011df335c1b5a49bd60e64d593ce7cdc
leneantonsen <lene.antonsen@uit.no>
2024-03-12 15:42:26 +0100

    for numeraltesting

---
+++ devtools/num_minip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smj:
+# sh devtools/noun_minip.sh GÅNÅGIS | less
+# sh devtools/noun_minip.sh tjielggidus 
+# Only get the lemma you ask for:
+# sh devtools/noun_minip.sh '^tjielggidus[:+]' 
+
+
+LOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cat test/data/numeraler.txt >$L_FILE
+
+P_FILE="test/data/testnumparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-smj/src/fst/generator-gt-norm.hfstol
+ done
+ rm -f $L_FILE
+done
+
